70bec13d69f73343ea07476a4320b0885a4fca66
Tazida <tazidarahman5@gmail.com>
2023-05-26 01:23:28 +0600

    added js

--- JS/dom.js
+++ JS/dom.js
@@ -1,6 +1,22 @@
 document.getElementById("apply-bg").addEventListener("click", function () {
   const friends = document.getElementsByClassName("friend");
   for (const friend of friends) {
-    console.log(friend);
+    friend.style.backgroundColor = "lightblue";
   }
+});
+document.getElementById("third").addEventListener("click", function () {
+  const third = document.getElementById("third_friend");
+  third.style.textAlign = "center";
+});
+
+document.getElementById("add").addEventListener("click", function () {
+  const friendContainer = document.getElementById("friend");
+  const friend = document.createElement("div");
+  friend.classList.add("friend");
+  friend.innerHTML = `
+  <h3 class="friend-name">New Friend</h3>
+  <p>Something new add</p> 
+  
+  `;
+  friendContainer.appendChild(friend);
 });
